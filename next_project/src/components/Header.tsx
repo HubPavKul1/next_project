66dbc86be2036709bd9640070d48048c4c8a5484
HubPavKul1 <kulpv@mail.ru>
2025-10-28 17:36:47 +0300

    add x-logo

--- next_project/src/components/Header.tsx
+++ next_project/src/components/Header.tsx
@@ -1,13 +1,12 @@
+import Image from "next/image";
 import Link from "next/link";
-import { IoLogoXing } from "react-icons/io";
+
 
 export function Header() {
   return (
     <header className="border-b border-white/10 px-6 py-4 flex items-center justify-between bg-black">
       <Link href="/" className="flex items-center gap-3">
-        {/* <Image alt="x-logo" src="" width={28} height={28}/> */}
-          <IoLogoXing />
-        {/* </Image> */}
+        <Image alt="x-logo" src="/x-logo.svg" width={28} height={28} priority/>
       </Link>
       <nav className="flex gap-4 text-sm text-white/80">
         <Link href="/">Home</Link>
